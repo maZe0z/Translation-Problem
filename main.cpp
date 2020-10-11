9dd1dbb6c42e590b1d827e3aba758f2a5e46906d
Zakhar <38567289+maZe0z@users.noreply.github.com>
2020-10-11 21:48:31 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double x;
+    cin >> x;
+    if (1<x<10){
+        cout << "pizda";
+    }
+    return 0;
+}
